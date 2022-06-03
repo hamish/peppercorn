8ed5ded439b60157a594dbad06835fdfa3a0fe81
hamish Currie <hamish@currie.com.au>
2022-06-03 17:08:59 +1000

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use peppercorn::run;
+use peppercorn::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()>{
+async fn main() -> std::io::Result<()> {
     let listner = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind random port");
     run(listner)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 fn spawn_app() -> String {
     let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listner.local_addr().unwrap().port();
-    let server = peppercorn::run(listner).expect("Failed to bind port");
+    let server = peppercorn::startup::run(listner).expect("Failed to bind port");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.01:{}", port)
 }
@@ -22,7 +22,7 @@ async fn health_check_works() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_200_for_valid_data(){
+async fn subscribe_returns_200_for_valid_data() {
     let address = spawn_app();
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -31,27 +31,34 @@ async fn subscribe_returns_200_for_valid_data(){
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
-        .await.expect("Fialed");
+        .await
+        .expect("Fialed");
     assert_eq!(200, response.status().as_u16());
 }
 
 #[tokio::test]
-async fn subscribe_returns_400_for_invalid_data(){
+async fn subscribe_returns_400_for_invalid_data() {
     let address = spawn_app();
     let client = reqwest::Client::new();
 
     let test_cases = vec![
         ("name=le$20guin", "missing email"),
         ("email=ursula_le_guin%40gmail.com", "missing name"),
-        ("", "missing both")
+        ("", "missing both"),
     ];
-    for (invalid_body, error_message) in test_cases{
-       let response = client
+    for (invalid_body, error_message) in test_cases {
+        let response = client
             .post(&format!("{}/subscriptions", &address))
             .header("Contnet-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
-            .await.expect("Fialed");
-        assert_eq!(400, response.status().as_u16(), "Api did not fail when {}", error_message);
+            .await
+            .expect("Fialed");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "Api did not fail when {}",
+            error_message
+        );
     }
 }
