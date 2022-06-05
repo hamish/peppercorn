6385db79343a09dcfb117e9352cc0cb7404f3750
hamish Currie <hamish@currie.com.au>
2022-06-05 18:52:29 +1000

    config partially working, updated github workflow

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,14 +1,51 @@
+use config::Config;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
+    pub application_port: u16,
 }
+
 #[derive(serde::Deserialize)]
-pub struct DatabaseSettings{
-    pub username:String,
-    pub password:String,
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
     pub port: u16,
     pub host: String,
-    pub database_name: String
+    pub database_name: String,
+}
+
+// impl DatabaseSettings {
+//     pub fn connection_string(&self) -> String {
+//         format!(
+//             "postgres://{}:{}@{}:{}/{}",
+//             self.username, self.password, self.host, self.port, self.database_name
+//         )
+//     }
+
+//     pub fn connection_string_without_db(&self) -> String {
+//         format!(
+//             "postgres://{}:{}@{}:{}",
+//             self.username, self.password, self.host, self.port
+//         )
+//     }
+// }
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // let mut settings = config::Config::default();
+
+    // settings.merge(config::File::with_name("configuration"))?;
+
+    // settings.try_into()
+    let settings = Config::builder()
+        // Add in `./Settings.toml`
+        .add_source(config::File::with_name("configuration"))
+        // Add in settings from the environment (with a prefix of APP)
+        // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
+        .add_source(config::Environment::with_prefix("APP"))
+        .build()
+        .unwrap();
+
+        settings
+            .try_deserialize::<Settings>()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use peppercorn::startup::run;
 use std::net::TcpListener;
+use peppercorn::startup::run;
+//use peppercorn::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listner = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind random port");
+    //let configuration = get_configuration().expect("Filed to get config");
+    let address = format!("127.0.0.1:{}", 8080);
+    let listner = TcpListener::bind(address)?;
     run(listner)?.await
 }
