b44188abc9140dedca49a33c43f4e4b076881a4d
hamish Currie <hamish@currie.com.au>
2022-06-13 08:01:16 +1000

    added very basic logging

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use peppercorn::configuration::get_configuration;
 use peppercorn::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Filed to get config");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
