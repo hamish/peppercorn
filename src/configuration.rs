679337d4220aec89be56a7c18293f6e0900de8e9
hamish Currie <hamish@currie.com.au>
2022-06-25 12:33:18 +1000

    initial docker implementation

--- src/configuration.rs
+++ src/configuration.rs
@@ -4,7 +4,13 @@ use secrecy::{Secret, ExposeSecret};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings{
+    pub port: u16,
+    pub host:String,
 }
 
 #[derive(serde::Deserialize)]
@@ -32,15 +38,51 @@ impl DatabaseSettings {
     }
 }
 
+pub enum Environment{
+    Local,
+    Production,
+}
+impl Environment{
+   pub fn as_str(&self) -> &'static str {
+       match self {
+           Environment::Local => "local",
+           Environment::Production => "production"
+       }
+   }
+}
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s:String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str(){
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. User local or production.", other
+            ))
+        }
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // let mut settings = config::Config::default();
-
     // settings.merge(config::File::with_name("configuration"))?;
-
     // settings.try_into()
+    let base_path=std::env::current_dir().expect("Unable to determine current directory");
+    let configuration_directory = base_path.join("configuration");
+let environment:Environment = std::env::var("APP_ENVIRONMENT")
+    .unwrap_or_else(|_| "local".into())
+    .try_into()
+    .expect("Failed to parse APP_ENVIRONMENT.")
+    ;
+
     let settings = Config::builder()
         // Add in `./Settings.toml`
-        .add_source(config::File::with_name("configuration"))
+        .add_source(config::File::from(
+            configuration_directory.join("base")
+        ).required(true))
+        .add_source(config::File::from(
+            configuration_directory.join(environment.as_str())
+        ).required(true))
         // Add in settings from the environment (with a prefix of APP)
         // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
         .add_source(config::Environment::with_prefix("APP"))
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use peppercorn::configuration::get_configuration;
 use peppercorn::startup::run;
 use peppercorn::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
 
@@ -11,10 +12,11 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Filed to get config");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to Connect to the DB.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listner = TcpListener::bind(address)?;
     run(listner, connection_pool)?.await
 }
